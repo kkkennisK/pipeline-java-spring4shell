49e8f7bd3b7cc23648758079c3fd478c20f34fc5
Administratorkk <kennis.kwan@systex.com.hk>
2025-02-26 09:48:28 +0800

    updated java code

--- src/test/java/com/reznok/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/reznok/helloworld/HelloworldApplicationTests.java
@@ -8,6 +8,14 @@ class HelloworldApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String apiKey = "12345-ABCDE";
+		System.out.println(apiKey);
+
+		String name = "John";
+        int age = 30;
+        System.out.printf("Name: %s, Age: %d%n", name, age);
+
 	}
 
 }
+
